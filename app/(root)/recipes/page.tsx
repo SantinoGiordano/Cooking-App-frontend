8a58bee182f48e5a19500d474a702c0edda0f37e
SantinoGiordano <santinogiordano13@gmail.com>
2026-06-03 12:45:10 -0400

    fixed the data issue

--- app/(root)/recipes/page.tsx
+++ app/(root)/recipes/page.tsx
@@ -1,31 +1,38 @@
-'use client'
+"use client";
 
 import { Recipe } from "@/app/types/types";
 import { useEffect, useState } from "react";
 
 export default function Recipes() {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-    const [recipes, setRecipes] = useState<Recipe[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/recipes")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRecipes(data.data);
+      })
+      .catch((error) => console.error("Error fetching recipes:", error));
+  }, []);
 
-    useEffect(() => {
-        fetch('/api/recipes')
-            .then(response => response.json())
-            .then(data => setRecipes(data))
-            .catch(error => console.error('Error fetching recipes:', error));
-    }, []);
-    
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Recipes page</h1>
-            <p>
-                {
-                    recipes.map(recipe => (
-                        <div key={recipe._id}>
-                            <h2>{recipe.name}</h2>
-                        </div>
-                    ))
-                }
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Recipes page</h1>
+      <div>
+        {recipes.map((recipe, index) => (
+          <div key={index}>
+            <h2>{recipe.name}</h2>
+            <p className="text-sm text-gray-500">
+              Instructions: {recipe.instructions}
             </p>
-        </div>
-    )
+            Prep Time: {recipe.prepTime} minutes <br />
+            <p className="text-sm text-gray-500">
+              Ingredients: {recipe.ingredients.join(", ")} <br />
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
