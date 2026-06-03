ac2c3f42a8f0671f8228389fc71bf89da2d29606
SantinoGiordano <santinogiordano13@gmail.com>
2026-06-03 09:24:42 -0400

    created a types page for all export types

---
+++ app/types/types.tsx
@@ -0,0 +1,10 @@
+export type Recipe = {
+    _id: string;
+    name: string;
+    ingredients: string[];
+    instructions: string;
+    prepTime: number;
+    cookTime: number;
+    difficulty: string;
+    cuisine: string;
+}
