64258009c2062df2ad528362b63d2e116ab7a6e0
SantinoGiordano <santinogiordano13@gmail.com>
2026-06-04 12:04:56 -0400

    installed and implemented the lucid react library

--- app/components/navButton.tsx
+++ app/components/navButton.tsx
@@ -1,24 +1,41 @@
+"use client";
+
+import { Search, House, Utensils, BookOpen, Compass, Plus } from "lucide-react";
+import Link from "next/link";
+
 export default function Page() {
   return (
     <>
       <div className="fab fab-flower">
-        {/* a focusable div with tabIndex is necessary to work on all browsers. role="button" is necessary for accessibility */}
         <div
           tabIndex={0}
-          role="button"
+          role="Link"
           className="btn btn-lg btn-circle btn-success"
         >
-          F
+          <Compass size={24} />
         </div>
 
-        {/* Main Action button replaces the original button when FAB is open */}
-        <button className="fab-main-action btn btn-circle btn-lg">M</button>
+        {/* Main Action */}
+        <button className="fab-main-action btn btn-circle btn-lg">
+          <Compass size={24} />
+        </button>
 
-        {/* buttons that show up when FAB is open */}
-        <button className="btn btn-lg btn-circle">A</button>
-        <button className="btn btn-lg btn-circle">B</button>
-        <button className="btn btn-lg btn-circle">C</button>
-        <button className="btn btn-lg btn-circle">D</button>
+        {/* FAB Links */}
+        <Link href={"/home"} className="btn btn-lg btn-circle">
+          <House size={22} />
+        </Link>
+
+        <Link href={"/search"} className="btn btn-lg btn-circle">
+          <Search size={22} />
+        </Link>
+
+        <Link href={"/myIngredients"} className="btn btn-lg btn-circle">
+          <Utensils size={22} />
+        </Link>
+
+        <Link href="/add-recipe" className="btn btn-lg btn-circle btn-primary">
+          <Plus size={22} />
+        </Link>
       </div>
     </>
   );
